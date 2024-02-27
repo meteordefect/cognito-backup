0784a6006d2c6faa597e30aaf1c42557574e0106
Person Name <mail@mail.com>
2024-02-27 17:15:16 +1100

    updates to lambda

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ resource "aws_s3_bucket" "cognito_backup_bucket" {
 
 resource "aws_s3_bucket_versioning" "cognito_backup_bucket_versioning" {
   bucket = aws_s3_bucket.cognito_backup_bucket.id
+  force_destroy = true 
   versioning_configuration {
     status = "Enabled"
   }
